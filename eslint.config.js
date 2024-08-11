67ed84dc45cb3a7e0df1b70a9acbad11aabdf9a9
DevVali <90271108+DevVali@users.noreply.github.com>
2024-08-12 00:43:56 +0200

    chore: add linting

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            semi: ['error', 'always'],
+            'no-var': 'error',
+            'prefer-const': 'error',
+            quotes: ['error', 'single', { avoidEscape: true }],
+            '@typescript-eslint/no-unused-expressions': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
+    { ignores: ['lib'] },
+];
